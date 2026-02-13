c15169a320296bb107c4cf092d1306d7b7638dfc
akshitapaliwal12-hue <akshita.12paliwal.12@gmail.com>
2026-02-13 14:37:51 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int fun(int n)  // Recursive 
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fun(n - 1) + fun(n - 2);
+}
+long int fun2(int n) // iterative code
+{
+    long int arr[n+1];
+    arr[0]= 0;
+    arr[1]=1;
+    for (int i= 2; i<= n; i++)
+    {
+        arr[i]= arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+}
+
+int main()
+{
+    printf("%d \n", fun(10));
+    printf("%d \n", fun2(10));
+}
